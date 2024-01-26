1dc2963082585ac3575db9eb15611e96d23836c6
WeedWorm075 <119393204+WeedWorm075@users.noreply.github.com>
2024-01-26 15:08:57 +0100

    j-4

---
+++ src/views/examples/Annonce.js
@@ -0,0 +1,39 @@
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Container,
+  Row,
+} from "reactstrap";
+// core components
+import VehiculeAxios from "components/Annonce/VehiculeAxios";
+
+const Annonce = () =>{
+    
+    return(
+        <>
+      {/* Page content */}
+      <Container className="main-content container-fluid mt--7" fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="bg-transparent">
+                <h3 className="mb-0">Annonces</h3>
+              </CardHeader>
+              <CardBody>
+                <Row className="icon-examples">
+                  <VehiculeAxios/>
+                  </Row>
+              </CardBody>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+           
+        </>
+    )
+}
+
+export default Annonce;
